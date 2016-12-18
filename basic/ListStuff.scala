6a621be7ad290de1bbcadc2318ca9ff1d816e736
andy robson <arobson73@yahoo.co.uk>
2016-12-18 20:50:43 +0000

    Update ListStuff.scala

--- basic/ListStuff.scala
+++ basic/ListStuff.scala
@@ -95,4 +95,45 @@ object List { // `List` companion object. Contains functions for creating and wo
   
   def reverse[A](as: List[A]): List[A] = foldLeft(as,List[A]())((x,y) => Cons(y,x))
 
+  def foldRightViaFoldLeft[A,B](as: List[A], z: B)(f:(B,A) => B): B = {
+    foldLeft(reverse(as),z)((b,a) => f(b,a))
+  }
+
+  def appendViaFoldRight[A](a: List[A], b: List[A]) : List[A] = foldRight(a,b)(Cons(_,_))
+  def appendViaFoldRight2[A](a: List[A], b: List[A]) : List[A] = foldRight(a,b)((x: A, y : List[A]) => Cons(x,y))
+  def appendViaFoldLeft[A](a: List[A], b: List[A]) : List[A] = foldLeft(reverse(a),b)((x:List[A],y:A) => Cons(y,x))
+  //List(List(1,2,3),List(4,5,6))
+  def concat[A](as: List[List[A]]): List[A] = foldRight(as,List[A]())((x: List[A], y: List[A]) => append(x,y)) 
+  def add1(as: List[Int]): List[Int] = foldRight(as,List[Int]())((x,y) => Cons(x+1,y))
+  def dupe[A](as: List[A]): List[A] = foldRight(as,List[A]())((x:A, acc: List[A]) => Cons(x,acc))
+  def dup2[A](as:List[A]):List[A] = foldRight(as,Nil:List[A])(Cons(_,_))
+
+  def double2String(as:List[Double]): List[String] = foldRight(as,Nil:List[String])((x,y) => Cons(x.toString,y))
+
+  def map[A,B](as:List[A])(f: A => B): List[B] = foldRight(as,Nil:List[B])((x:A,y:List[B]) => Cons(f(x),y))
+
+  def filter[A](as:List[A])(f: A => Boolean): List[A] =
+    foldRight(as,Nil:List[A])((x:A,acc:List[A]) => 
+      if (f(x)) Cons(x,acc)
+      else acc
+    )
+  //flatMap(List(1,2,3))(x => List(x*2))
+  //
+  //flatMap(List(1,2,3))(x => List(x*2,x*4))
+  def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] =  concat(map(as)(f)) 
+  def filterWithFlatMap[A](as: List[A])(f:A => Boolean): List[A] = flatMap(as)(x => if (f(x)) List(x) else Nil)
+
+  def addLists(as: List[Int], bs: List[Int]) : List[Int] = (as,bs) match {
+    case (Nil,_) => bs
+    case (_,Nil) => as
+    case(Cons(x,xs),Cons(y,ys)) => Cons(x+y,addLists(xs,ys))
+  }
+
+  def zipWith[A,B](as: List[A], bs: List[A])(f: (A,A) => A): List[A] = (as,bs) match {
+    case (Nil,_) => bs
+    case (_,Nil) => as
+    case (Cons(x,xs),Cons(y,ys)) => Cons(f(x,y),zipWith(xs,ys)(f))
+  }
+
+  
 }
