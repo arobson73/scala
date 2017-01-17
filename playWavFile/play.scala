9c0c8b13e6f205d8985e325f66354e74cdf4bcf6
AR <arobson73@yahoo.co.uk>
2017-01-17 19:32:41 +0000

    Create play.scala

---
+++ playWavFile/play.scala
@@ -0,0 +1,44 @@
+import javax.sound.sampled._
+
+object TestSound  {
+  //Input:   scala TestSound -10
+  def main(args: Array[String]): Unit = {
+    val oneMinute = 60*1000
+
+    // usage
+    if (args.length < 1) showUsageAndExit
+
+    // initialize the values from the user input
+    val minutesToWait = args(0).toInt
+    val gainControl = if (args.length==2) args(1).toInt else -20
+
+    println(s"Timer started. Wait time is $minutesToWait minutes.\n")
+
+    // wait the desired time
+    for (i <- 1 to minutesToWait) {
+      Thread.sleep(oneMinute)
+      println(s"time remaining: ${minutesToWait-i} ...")
+    }
+
+    // play the sound twice
+    for (i <- 1 to 2) {
+      playSoundfile("notify.wav",gainControl) //had this wav in same directory as compile class file output
+      Thread.sleep(7*1000)
+    }
+  }
+  def playSoundfile(f: String, gainControl: Int) {
+    val audioInputStream = AudioSystem.getAudioInputStream(new java.io.File(f))
+    val clip = AudioSystem.getClip
+    clip.open(audioInputStream)
+    val floatGainControl = clip.getControl(FloatControl.Type.MASTER_GAIN).asInstanceOf[FloatControl]
+    floatGainControl.setValue(gainControl) //reduce volume by x decibels (like -10f or -20f)
+    clip.start
+  }
+
+  def showUsageAndExit {
+    Console.err.println("Usage: timer minutes-before-alarm <gain-control>")
+    Console.err.println(" gain-control should be something like -10 or -20")
+    System.exit(1)
+  }
+
+}
